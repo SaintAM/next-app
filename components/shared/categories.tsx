a44d16f82173ab5b6eb279599ed05e001b4914c9
SaintAM <mistersteepstar@gmail.com>
2024-10-07 22:25:56 +0300

    first

---
+++ components/shared/categories.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+interface Props {
+  className?: string;
+}
+const cats = ["Desert", "Myaso", "Vegan", "Myaso", "Napitki", "Myaso", "Vegan"];
+const activeIndex = 0;
+
+export const Categories: React.FC<Props> = ({ className }) => {
+  return (
+    <div
+      className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)}
+    >
+      {cats.map((cat, i) => (
+        <a
+          key={i}
+          className={cn(
+            "flex items-center font-bold h-11 rounded-2xl px-5",
+            i === activeIndex &&
+              "bg-white shadow-md shadow-gray-200 text-primary"
+          )}
+          href={``}
+        >
+          <button>{cat}</button>
+        </a>
+      ))}
+    </div>
+  );
+};
